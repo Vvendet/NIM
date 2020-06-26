daacc746260780c06053cda4d608bfa810738e67
V? <66224435+Vvendet@users.noreply.github.com>
2020-06-26 18:01:56 -0300

    Update jogo.php

--- jogo.php
+++ jogo.php
@@ -1,69 +1,70 @@
 <?php # -*- coding: utf-8 -*-
 
 
-function campeonato():
+function campeonato(){
 
     # Pontuações:
-    usuario = 0
-    computador = 0
+    $usuario = 0;
+    $computador = 0;
 
     # Executa 3 vezes:
-    for _ in range(3):
+    for $_ in range(3):
 
         # Executa a partida:
-        vencedor = partida()
+        $vencedor = partida();
 
         # Verifica o resultado, somando a pontuação:
-        if vencedor == 1:
+        if $vencedor == 1:
             # Usuário venceu:
-            usuario = usuario + 1
+            $usuario = $usuario + 1;
         else:
             # Computador venceu:
-            computador = computador + 1
+            $computador = $computador + 1;
 
     # Exibe o placar final:
     echo("Placar final: Você  {} x {}  Computador".format(usuario, computador));
+}
 
 function jogo(){
     echo("Bem-vindo ao jogo do NIM! Escolha:");
-    escolha = int(input("1 - Partida isolada  2 - Campeonato:  "))
-    if escolha == 1:
-        return partida()
-    elif escolha == 2:
-        campeonato()
+    $escolha = int(input("1 - Partida isolada  2 - Campeonato:  "));
+    if $escolha == 1:;
+        return partida();
+    elseif $escolha == 2:
+        campeonato();
     else:
         
-        while escolha != 1 or escolha != 2:
+        while $escolha != 1 or $escolha != 2:
             echo("Entrada inválida");
-            escolha = int(input("1 - Partida isolada  2 - Campeonato:  "))
+            $escolha = int(input("1 - Partida isolada  2 - Campeonato:  "))
 }
 
 function partida(){
-    n = int(input("Quantas peças? "))
-    m = int(input("Limite de peças por jogada? " ))
-    if n == 2 and m == 3:
+    $n = int(input("Quantas peças? "))
+    $m = int(input("Limite de peças por jogada? " ))
+    if $n == 2 and $m == 3:
         echo("Computador começa!");
-        while n >0:
-            jogada = computador_escolhe_jogada(n, m)
+        while $n >0:
+            $jogada = computador_escolhe_jogada(n, m);
             echo("O computador tirou ",jogada," peças!");           
-            n = n - jogada
+            $n = $n - $jogada
             echo("Agora restam ", n," peças no tabuleiro!");
-            if n == 0:
+            if $n == 0:
                 echo("Computador venceu!");
 
-            if n > 0:
-                jogada = usuario_escolhe_jogada(n, m)
-                n = n-jogada        
+            if $n > 0:
+                $jogada = usuario_escolhe_jogada(n, m);
+                $n = $n-$jogada ;       
                 echo("Agora restam ", n," peças no tabuleiro!");
-                if n == 0:
+                if $n == 0:
                     echo("Voce venceu!");
 
-    elif n<m:
+    elseif n<m:
         while n < m:
             echo("entrada inválida");
             m = int(input("Limite de peças por jogada? " ))
 
-    elif  n == m: 
+    elseif  n == m: 
         echo("Computador começa!");
         while n >0:
             jogada = computador_escolhe_jogada(n, m)
@@ -81,7 +82,7 @@ function partida(){
                     echo("Voce venceu!");
 
 
-    elif n == 9 and m == 2:
+    elseif n == 9 and m == 2:
         echo("Você começa!");
         while n > 0:  
             jogada = usuario_escolhe_jogada(n, m)
@@ -98,7 +99,7 @@ function partida(){
                 if n == 0:
                     echo("Computador venceu!");
 
-    elif n == 11 and m == 3:
+    elseif n == 11 and m == 3:
         echo("Computador começa!");
         while n >0:
             jogada = computador_escolhe_jogada(n, m)
@@ -116,7 +117,7 @@ function partida(){
                     echo("Voce venceu!");
 
 
-    elif n == 5 and m == 3:
+    elseif n == 5 and m == 3:
         echo("Computador começa!");
         while n >0:
             jogada = computador_escolhe_jogada(n, m)
@@ -136,7 +137,7 @@ function partida(){
 
 
             
-    elif n%(m+1) or n%m==0:
+    elseif n%(m+1) or n%m==0:
         echo("Você começa!");
         while n > 0:  
             jogada = usuario_escolhe_jogada(n, m)
@@ -156,11 +157,11 @@ function partida(){
     echo("Fim da partida");
 }   
 function usuario_escolhe_jogada(n,m){
-    jogada = int(input("Quantas peças você vai tirar? "))
-    while jogada > m or jogada <= 0 or jogada > n:
+    $jogada = int(input("Quantas peças você vai tirar? "))
+    while $jogada > $m or $jogada <= 0 or $jogada > $n:
         echo("entrada inválida");
-        jogada = int(input("Quantas peças você vai tirar? "));
-    return jogada
+        $jogada = int(input("Quantas peças você vai tirar? "));
+    return $jogada
 }
 function computador_escolhe_jogada(n, m){
 
@@ -168,21 +169,21 @@ function computador_escolhe_jogada(n, m){
     echo("Vez do computador!");
 
     # Pode retirar todas as peças?
-    if n <= m:
+    if $n <= $m:
 
         # Retira todas as peças e ganha o jogo:
-        return n
+        return $n; 
 
     else:
 
         # Verifica se é possível deixar uma quantia múltipla de m+1:
-        quantia = n % (m+1)
+        $quantia = $n % (m+1);
 
-        if quantia > 0:
-            return quantia
+        if $quantia > 0:
+            return $quantia;
 
         # Não é, então tire m peças:
-        return m
+        return $m;
 
     
 }
